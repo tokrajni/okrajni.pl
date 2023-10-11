2adc1c409b81ccbe8c3464283db61434487d751d
Tomek Okrajni <tomasz.okrajni2@gmail.com>
2023-10-11 14:46:11 +0200

    Add simple home page

---
+++ src/main/java/com/okrajni/okrajnipl/controller/FrontController.java
@@ -0,0 +1,14 @@
+package com.okrajni.okrajnipl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+
+}
