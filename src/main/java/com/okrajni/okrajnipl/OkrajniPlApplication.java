681bc5ce96e2e0959455411c1c8f61d2f9b28780
Tomek Okrajni <tomasz.okrajni2@gmail.com>
2023-10-11 11:38:15 +0200

    initial commit

---
+++ src/main/java/com/okrajni/okrajnipl/OkrajniPlApplication.java
@@ -0,0 +1,13 @@
+package com.okrajni.okrajnipl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrajniPlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrajniPlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okrajni/okrajnipl/OkrajniPlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okrajni.okrajnipl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkrajniPlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
